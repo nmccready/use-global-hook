4e219508b6dea286a2f424b89b4f057722996689
nmccready <nemtcan@gmail.com>
2019-08-21 17:31:54 -0400

    test example

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,19 @@
+import React from 'react';
 import 'jest-extended';
 import 'jest-expect-message';
+import useGlobalHook, { Store } from '.';
 
 describe('useGlobalHook', () => {
-  it('should ', () => {});
+  it('not complain about hookWork', () => {
+    interface SomeState {
+      a?: string;
+    }
+    interface OuterActions<T> {
+      setA: (newState: T) => void;
+    }
+    interface InnerActions<T> {
+      setA: (_: Store<T, OuterActions<T>>, newState: T) => void;
+    }
+    useGlobalHook<SomeState, InnerActions<SomeState>, OuterActions<SomeState>>({ React });
+  });
 });
