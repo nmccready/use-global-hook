45dcc0bc5a0bef76cab8b5309ae4df9cbb2be3f2
nmccready <nemtcan@gmail.com>
2019-08-23 11:16:33 -0400

    feat: umd artifacts

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const umd = require('gulp-umd');
 const del = require('del');
 const { compilerOptions: tsconfig } = require('./tsconfig.json');
 
 gulp.task('clean', () => del(['.tmp', 'lib']));
+
+gulp.task('umd', () =>
+  gulp
+    .src('lib/*.js')
+    .pipe(umd())
+    .pipe(gulp.dest('umd'))
+);
 
 const build = (
   src = [
@@ -21,4 +29,4 @@ const build = (
 
 gulp.task('build', build());
 
-gulp.task('default', gulp.series('clean', gulp.parallel('build')));
+gulp.task('default', gulp.series('clean', gulp.series('build', 'umd')));
