c3fdc482f6ae84e53d9d3f0a2e1bd173c082960b
nmccready <nemtcan@gmail.com>
2019-08-27 16:22:49 -0400

    gh-pages site via x0

---
+++ docs/website/_app.js
@@ -0,0 +1,85 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import * as scope from '@compositor/x0/components';
+import { Provider as RebassProvider } from 'rebass'; // Flex, Box, Container
+// import sortBy from 'lodash.sortby';
+
+import LandingLayout from './_landing';
+import theme from './_theme';
+import Logo from './_logo';
+
+const { ScopeProvider, SidebarLayout } = scope;
+
+// const navOrder = [
+//   'index',
+//   'getting-started',
+//   'markdown',
+//   'react',
+//   'MDX',
+//   'JSX',
+//   'routing',
+//   'custom-app',
+//   'components',
+//   'ScopeProvider',
+//   'SidebarLayout',
+//   'LivePreview',
+//   'LiveEditor',
+//   'cli-options',
+//   'exporting',
+//   'examples',
+// ];
+const pageNames = {
+  index: 'Home',
+  'cli-options': 'CLI Options',
+};
+
+const hardRoutes = [
+  {
+    key: './api.md',
+    name: 'Api',
+    href: 'https://localhost:9090',
+    path: 'https://localhost:9090',
+  },
+  {
+    key: './coverage.md',
+    name: 'Coverage',
+    href: 'https://coveralls.io/github/nmccready/use-global-hook?branch=master',
+    path: 'https://coveralls.io/github/nmccready/use-global-hook?branch=master',
+  },
+];
+
+const sortRoutes = (routes) =>
+  routes
+    .map((route) => {
+      if (!pageNames[route.name]) return route;
+      return {
+        ...route,
+        name: pageNames[route.name],
+      };
+    })
+    .concat(hardRoutes);
+
+const App = (props) => {
+  const { routes, route } = props;
+  const { layout } = (route && route.props) || {};
+
+  const Layout = layout === 'landing' ? LandingLayout : SidebarLayout;
+
+  // ! all routes can be custom sorted again by the doc consumer
+  // ! they come pre-sorted, see @compositor/x0/src/entry.js
+  const nav = sortRoutes(routes);
+
+  return (
+    <ScopeProvider scope={scope}>
+      <RebassProvider theme={theme}>
+        <Layout {...props} title="use-global-hook" routes={nav} logo={<Logo size={24} />} />
+      </RebassProvider>
+    </ScopeProvider>
+  );
+};
+
+App.defaultProps = {
+  title: 'use-global-hook',
+};
+
+export default App;
--- gulpfile.ts
+++ gulpfile.ts
@@ -2,6 +2,8 @@ import gulp from 'gulp';
 import ts from 'gulp-typescript';
 import del from 'del';
 import run from 'gulp-run';
+import replace from 'gulp-replace';
+import rename from 'gulp-rename';
 
 /*
 Gulp is for an itemized build so you can easily
@@ -15,7 +17,15 @@ const pkg = require('./package.json');
 const getDir = (fileName: string) => fileName.split('/')[0];
 
 gulp.task('clean', () =>
-  del(['.tmp', getDir(pkg.main), '*.log', getDir(pkg.umd), getDir(pkg.module), 'docs'])
+  del(['.tmp', getDir(pkg.main), '*.log', getDir(pkg.umd), getDir(pkg.module)])
+);
+
+gulp.task('docs:clean', () =>
+  del(['docs/public', 'docs/api_website', 'docs/website/*.md?(x)'])
+);
+
+gulp.task('docs:clean:use-global-hook-api-website', () =>
+  del(['../use-global-hook-api-website/**/*', '!../use-global-hook-api-website/.gitignore'])
 );
 
 const build = (
@@ -34,6 +44,45 @@ const build = (
 
 gulp.task('build', build());
 
+gulp.task('docs:website', () => run('yarn x0 docs').exec());
+
+const docsSource = ['**/*.md?(x)', '!docs/**/*', '!node_modules/**/*'];
+
+gulp.task(
+  'docs:website',
+  gulp.series('docs:clean', () =>
+    gulp
+      .src(docsSource)
+      .pipe(replace(/\/index.md/g, '')) // fix links
+      .pipe(
+        rename((filePath) => {
+          if (filePath.basename.toUpperCase() === 'README') {
+            filePath.basename = 'index';
+            if (filePath.dirname === '.') {
+              // we're at root README
+              filePath.basename = 'docs';
+            }
+          }
+        })
+      )
+      .pipe(gulp.dest('docs/website'))
+  )
+);
+
+gulp.task(
+  'docs:watch',
+  gulp.series('docs:website', () => {
+    gulp.watch(docsSource, gulp.series('docs:website'));
+  })
+);
+
 gulp.task('docs:api', () => run('yarn docs:api').exec());
+
+gulp.task('docs:api', () =>
+  gulp.series(
+    run('yarn docs:api:website').exec(),
+    gulp.src('docs/api_website/**/*').pipe(gulp.dest('../use-global-hook-api-website'))
+  )
+);
 
 gulp.task('default', gulp.series('clean', gulp.parallel('build', 'docs:api')));
