8ce7f4278a1b524bf59c28bad5e4b5fc8787b556
nmccready <nemtcan@gmail.com>
2019-08-28 08:33:07 -0400

    removing x0 to just keep simple api site

--- gulpfile.ts
+++ gulpfile.ts
@@ -20,22 +20,12 @@ gulp.task('clean', () =>
   del(['.tmp', getDir(pkg.main), '*.log', getDir(pkg.umd), getDir(pkg.module)])
 );
 
-gulp.task('docs:clean', () =>
-  del(['docs/public', 'docs/api_website', 'docs/website/*.md?(x)'])
-);
-
-gulp.task('docs:clean:api:website', () =>
-  del(['../use-global-hook-api-website/**/*', '!../use-global-hook-api-website/.gitignore'], {
-    force: true,
-  })
-);
-
 const build = (
   src = [
     'src/**/*.ts?(x)',
     '!src/**/*.spec.ts?(x)',
     '!src/**/*.test.ts?(x)',
-    '!src/test/**/*',
+    '!src/test/**/*'
   ],
   dest = 'lib'
 ) => () =>
@@ -46,57 +36,18 @@ const build = (
 
 gulp.task('build', build());
 
+const docsClean = () => del(['docs']);
+
 const docsSource = ['**/*.md?(x)', '!docs/**/*', '!node_modules/**/*'];
 
-const setupWebsiteMarkdown = () =>
-  gulp
-    .src(docsSource)
-    .pipe(replace(/\/index.md/g, '')) // fix links
-    .pipe(
-      rename((filePath) => {
-        if (filePath.basename.toUpperCase() === 'README') {
-          filePath.basename = 'index';
-          if (filePath.dirname === '.') {
-            // we're at root README
-            filePath.basename = 'docs';
-          }
-        }
-      })
-    )
-    .pipe(gulp.dest('docs/website'));
+const docsApiWebsite = () =>
+  run('yarn docs:api:website', { verbosity: 3 }).exec();
 
-const runDevWebsite = () => run('yarn x0 docs/website', { verbosity: 3 }).exec();
-
-const publishMainWebsite = () => run('yarn website', { verbosity: 3 }).exec();
-
-const publishApiWebsite = () => run('yarn publish:api:website', { verbosity: 3 }).exec();
-
-gulp.task('docs:website', gulp.series('docs:clean', setupWebsiteMarkdown));
-
-gulp.task(
-  'docs:website:dev',
-  gulp.series('docs:website', runDevWebsite, () => {
-    gulp.watch(docsSource, gulp.series('docs:website', runDevWebsite));
-  })
-);
-
-gulp.task('docs:api:markdown', () => run('yarn docs:api:markdown').exec());
-
-gulp.task(
-  'docs:api:website',
-  gulp.series(
-    () => run('yarn docs:api:website').exec(),
-    () => gulp.src('docs/api_website/**/*').pipe(gulp.dest('../use-global-hook-api-website'))
-  )
-);
+const docsApiMarkdown = () => run('yarn docs:api:markdown').exec();
 
 gulp.task('default', gulp.series('clean', 'build'));
 
 gulp.task(
-  'publish:websites',
-  gulp.parallel(
-    gulp.series('docs:api:website', publishApiWebsite),
-    'docs:api:markdown',
-    gulp.series('docs:website', publishMainWebsite)
-  )
+  'docs',
+  gulp.series(docsClean, gulp.parallel(docsApiMarkdown, docsApiWebsite))
 );
